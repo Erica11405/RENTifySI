42efcbaafe4a93de163e6bb710863ecb87131040
erica <ericaaquino0114@gmail.com>
2025-11-27 17:53:04 +0800

    Deployment 2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,9 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import dj_database_url
 import os
 
+from decouple import config, Csv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -81,10 +82,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    'default': dj_database_url.parse(
+            config('DATABASE_URL', default='sqlite:///' + str(BASE_DIR / 'db.sqlite3')),
+            conn_max_age=600,
+            conn_health_checks=True,
+        )
 }
 
 
